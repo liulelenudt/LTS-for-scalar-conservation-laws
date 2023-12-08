0f2186fac64e2e234d0c5c843e8ce30aa5a41212
liulelenudt <148626828+liulelenudt@users.noreply.github.com>
2023-12-08 16:17:04 +0800

    Delete Example 4.5 directory

--- Example 4.5/BurgersLF.m
+++
@@ -1,7 +0,0 @@
-function [numflux] = BurgersLF(u,v,lambda,maxvel)
-% function [numflux] = BurgersLF(u,v,lambda,maxvel);
-% Purpose: Evaluate the Lax Friedrich numerical flux for Burgers equation
-
-fu = u.^2/2; fv = v.^2/2;
-numflux = (fu+fv)/2 - maxvel/2*(v-u);
-end
