f0de30b7626ad26692999b721dfe7696f81b4d64
liulelenudt <148626828+liulelenudt@users.noreply.github.com>
2023-12-10 19:15:24 +0800

    Delete Computational efficiency directory

--- Computational efficiency/pRRK54/relax.m
+++
@@ -1,9 +0,0 @@
-function [k_res,kappa_res] = relax(h,k_hat,FinalTime)
-syms k_res
-kappa_res = 1/h-1.508/k_res;
-k_res = vpasolve((1+k_res*kappa_res+1/2*(k_res*kappa_res)^2+1/6*(k_res*kappa_res)^3+5*64/14293.0*(k_res*kappa_res)^4)/...
-                 (1+k_res*kappa_res+1/2*(k_res*kappa_res)^2+1/6*(k_res*kappa_res)^3+1/24*(k_res*kappa_res)^4+64/14293*(k_res*kappa_res)^5)-...
-                 (FinalTime-fix(FinalTime/k_hat)*k_hat)/k_res);
-k_res = k_res(3);
-kappa_res = max(1/h-1.508/k_res,0);
-end
